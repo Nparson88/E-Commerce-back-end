4cf0d4438ff2f26383becb2f256e64a4b4953bb6
Nicholas Parson <Nparson88@gmail.com>
2021-05-24 23:02:36 -0400

    spelling errors

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -18,7 +18,7 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
-  Tag.FindOne({
+  Tag.findOne({
     where: { id: req.params.id },
     include: { model: Product }
   }).then(tagData => res.json(tagData))
